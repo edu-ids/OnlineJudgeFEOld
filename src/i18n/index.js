2b6f41db754b87c0c7ac92646b8a6072df5fe4c4
mizphses <fuminori@mizphs.es>
2024-04-25 00:32:54 +0900

    変数名ミス

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -4,12 +4,12 @@ import VueI18n from 'vue-i18n'
 import ivenUS from 'iview/dist/locale/en-US'
 import ivzhCN from 'iview/dist/locale/zh-CN'
 import ivzhTW from 'iview/dist/locale/zh-TW'
-import ivzhJA from 'iview/dist/locale/ja-JP'
+import ivjaJP from 'iview/dist/locale/ja-JP'
 // element UI
 import elenUS from 'element-ui/lib/locale/lang/en'
 import elzhCN from 'element-ui/lib/locale/lang/zh-CN'
 import elzhTW from 'element-ui/lib/locale/lang/zh-TW'
-import elzhJA from 'element-ui/lib/locale/lang/ja'
+import eljaJP from 'element-ui/lib/locale/lang/ja'
 
 Vue.use(VueI18n)
 
@@ -17,7 +17,7 @@ const languages = [
   {value: 'en-US', label: 'English', iv: ivenUS, el: elenUS},
   {value: 'zh-CN', label: '简体中文', iv: ivzhCN, el: elzhCN},
   {value: 'zh-TW', label: '繁體中文', iv: ivzhTW, el: elzhTW},
-  {value: 'ja-JP', label: '日本語', iv: ivzhJA, el: elzhJA}
+  {value: 'ja-JP', label: '日本語', iv: ivjaJP, el: eljaJP}
 ]
 const messages = {}
 
